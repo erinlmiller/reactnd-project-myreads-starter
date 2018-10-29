7b897c0c2a4d012e1de6d5fe60cb37f7d37141ff
Erin Miller <erinthered324@gmail.com>
2018-10-29 18:15:56 -0400

    Clean up code

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 class Book extends React.Component {
     render () {
+      const { book } = this.props;
         return (
             <li>
             <div className="book">
               <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${this.props.book.imageLinks && this.props.book.imageLinks.thumbnail || ""}")` }}></div>
+                <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${(book.imageLinks && book.imageLinks.thumbnail) || ""}")` }}></div>
                 <div className="book-shelf-changer">
-                  <select value={this.props.book.shelf || "none"} onChange={(e) => { this.props.updateBook(this.props.book, e.target.value) }}>
+                  <select value={book.shelf || "none"} onChange={(e) => { this.props.updateBook(book, e.target.value) }}>
                     <option value="move" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
@@ -17,8 +18,8 @@ class Book extends React.Component {
                   </select>
                 </div>
               </div>
-              <div className="book-title">{this.props.book.title}</div>
-              <div className="book-authors">{this.props.book.authors && this.props.book.authors[0] || "No Author..."}</div>
+              <div className="book-title">{book.title}</div>
+              <div className="book-authors">{(book.authors && book.authors[0]) || "No Author"}</div>
             </div>
           </li>
         );
